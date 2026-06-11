ce21e70435d55c851c9fa4390f30490325c8bdad
JosepNavarroLlana <jnavarrollana@gmail.com>
2026-06-11 12:51:02 +0200

    fix: force https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,12 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+    use Illuminate\Support\Facades\URL;
+
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
