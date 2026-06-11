67e92855165e2a962054909f1d5063e9d4a26f1e
JosepNavarroLlana <jnavarrollana@gmail.com>
2026-06-11 13:17:05 +0200

    fix: firstOrCreate

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -10,7 +10,7 @@ class AdminSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
+        User::firstOrCreate([
             'name' => 'Admin',
             'email' => 'admin@cine.com',
             'password' => Hash::make('admin1234'),
